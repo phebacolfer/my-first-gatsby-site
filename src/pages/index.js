fcd56087fcbe074bee8afb5f45eaf931fcdb6a2f
Pheba <phebajcolfer@gmail.com>
2023-02-16 18:43:07 -0500

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../pages/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="A small bundle of pink flowers surrounded by green leaves and flower buds"
+        src="../images/flowers.jpg"
+      />
     </Layout>
   )
 }
